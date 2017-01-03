dbd314c16210a00297aa0152c392d0e420c57a6b
rohanssrao <rohanssrao@gmail.com>
2017-01-02 21:57:44 -0500

    Lots of stuff

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,5 +1,9 @@
 var app = angular.module('PCHS-Webmaster', ['ngAnimate', 'ngMaterial']);
 
-app.controller('AppController', ['$scope', function($scope) {
+app.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+
+	$scope.openMenu = function() {
+		$mdSidenav("left").toggle()
+	}
 
 }]);
